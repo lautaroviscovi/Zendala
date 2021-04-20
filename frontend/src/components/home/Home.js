bdc8521be92dabd41d509e1d350acfd36bcc0917
lautaroviscovi <lautaro.v@revelaciondata.com.ar>
2021-04-20 00:00:15 -0300

    table is done

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -21,7 +21,7 @@ export const Home = () => {
             'x-token': token
         }
         }).then((response) => {
-            setCustomers({ users: response.data });
+            setCustomers( { users: response.data } );
         });
     };
     
@@ -58,8 +58,8 @@ export const Home = () => {
                 <th>Email</th>
                 <th>Country code</th>
                 <th>State</th>
-                <th>Line 1</th>
                 <th>City</th>
+                <th>ZIP</th>
                 <th>Phone number</th>
               </tr>
             </thead>
@@ -67,66 +67,45 @@ export const Home = () => {
             {customers.users.map( ( customer ) => (
                             <tr key={customer.id}>
                                 <td>{customer.name}</td>
+                                <td>{customer.last_name}</td>
+                                <td>{customer.email}</td>
+                                <td>{customer.country_code}</td>
+                                <td>{customer.state}</td>
+                                <td>{customer.city}</td>
+                                <td>{customer.postal_code}</td>
+                                <td>{customer.phone_number}</td>
                             </tr>
                         ))}
-              <tr>
-              {customers.users.map( ( customer ) => (
-                            <tr key={customer.id}>
-                                <td>{customer.name}</td>
-                            </tr>
-                        ))}
-              </tr>
-              <tr>
-                <td>Lorem ipsum - cell A2</td>
-                <td>Lorem ipsum - cell B2</td>
-                <td>Lorem ipsum - cell C2</td>
-                <td>Lorem ipsum - cell D2</td>
-                <td>Lorem ipsum - cell E2</td>
-              </tr>
-              <tr>
-                <td>Lorem ipsum - cell A3</td>
-                <td>Lorem ipsum - cell B3</td>
-                <td>Lorem ipsum - cell C3</td>
-                <td>Lorem ipsum - cell D3</td>
-                <td>Lorem ipsum - cell E3</td>
-              </tr>
-              <tr>
-                <td>Lorem ipsum - cell A4</td>
-                <td>Lorem ipsum - cell B4</td>
-                <td>Lorem ipsum - cell C4</td>
-                <td>Lorem ipsum - cell D4</td>
-                <td>Lorem ipsum - cell E4</td>
-              </tr>
-              <tr>
-                <td>Lorem ipsum - cell A5</td>
-                <td>Lorem ipsum - cell B5</td>
-                <td>Lorem ipsum - cell C5</td>
-                <td>Lorem ipsum - cell D5</td>
-                <td>Lorem ipsum - cell E5</td>
-              </tr>
-              <tr>
-                <td>Lorem ipsum - cell A6</td>
-                <td>Lorem ipsum - cell B6</td>
-                <td>Lorem ipsum - cell C6</td>
-                <td>Lorem ipsum - cell D6</td>
-                <td>Lorem ipsum - cell E6</td>
-              </tr>
             </tbody>
           </table>
         </div>
       </div>
+    )
+};
 
-        )
-    };
-    // <div>
-    //     <tbody>
-    //                     {customers.users.map((cliente) => (
-    //                         <tr key={cliente.id}>
-    //                             <td>{cliente.name}</td>
-    //                             <td>{cliente.last_name}</td>
-    //                             <td>{cliente.email}</td>
-    //                             <td>{cliente.phone_number}</td>
-    //                         </tr>
-    //                     ))}
-    //                 </tbody>
-    // </div>
+
+
+
+
+// const [ customers, setCustomers]  = useState({ users: [] });
+    
+//     const get = () => {
+//         const url = `${ baseUrl }/auth/users`;
+    
+//         const token = localStorage.getItem( 'token' ) || '';
+        
+//         fetch(url, {
+//         headers: {
+//             'Content-type': 'application/json',
+//             'x-token': token
+//         }
+//         }).then((response) => {
+//             setCustomers( { users: response.data } );
+//         });
+//     };
+    
+//     useEffect(() => {
+        
+//         get();        
+    
+//     }, [] ); 
